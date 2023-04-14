fb29b19c5a4f16db5e8cbb846eacc768ed74b232
Arthur Bueno Vilas Boas <arthurbvb@hotmail.com>
2023-04-13 21:50:17 -0300

    novas aulas

---
+++ exercicios/desafio08.py
@@ -0,0 +1,3 @@
+n1= int(input('digite quandos metros voce quer  converter: '))
+
+print ('O valor em Cm é {} e em Mm é {}'.format(n1*100, n1*1000))
---
+++ exercicios/desafio10.py
@@ -0,0 +1,2 @@
+n= float(input('Quanto dinheiro voce tem? '))
+print('Voce pode comprar {} dolares'.format(n/3.27))
