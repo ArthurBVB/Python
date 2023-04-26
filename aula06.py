05d09fc39d6ae2a04ded9528f1660b5ae1f24d13
Arthur Bueno Vilas Boas <arthurbvb@hotmail.com>
2023-04-25 21:43:31 -0300

    nova aula

---
+++ aula06.py
@@ -0,0 +1,20 @@
+import math
+
+num = int (input('Digite um numero = '))
+raiz = math.sqrt(num)
+
+print ('A raiz de {} é {}'.format(num,math.floor(raiz)))
+
+from math import sqrt
+
+vrt = int (input('Digite um numero= '))
+rais = sqrt((vrt))
+
+print ('A raiz é {}'.format(rais))
+
+import random
+
+xz = random.randint(1,10)
+print (xz)
+
+
