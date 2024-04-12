05e9307675a0d4a68fe1a7fcbdf29a4f3719ffff
Arthur Bueno Vilas Boas <arthurbvb@hotmail.com>
2024-04-12 16:29:31 -0300

    teste
    teste

--- exercicios/dasafio14.py
+++ exercicios/dasafio14.py
@@ -1,3 +1,3 @@
-c = float (input('digite o valor da temperatura em celcius = '))
+c = float (input("digite o valor da temperatura em celcius = "))
 f= 9*c/5+32
-print('A atemmperatura de {:2} C corresponde a {:2} F'.format(c,f))
+print("A atemmperatura de {:2} C corresponde a {:2} F".format(c,f))
